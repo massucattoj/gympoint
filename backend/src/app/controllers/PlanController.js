6f03c90a652b33c2ee3b12b93cf5dead04a7a878
massucattoj <massucattoj@gmail.com>
2019-12-05 23:32:43 -0300

    update validation in create plan

--- backend/src/app/controllers/PlanController.js
+++ backend/src/app/controllers/PlanController.js
@@ -3,7 +3,32 @@ import Plan from '../models/Plan';
 
 class PlanController {
   async store(req, res) {
-    // const { id, title, duration, price } = await Plan.create(req.body);
+    /**
+     * Create new Plans
+     */
+
+    // Ensure fields and type fields
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number()
+        .required()
+        .integer(),
+      price: Yup.number().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    // Verify with title already exist
+    const titleExists = await Plan.findOne({
+      where: { title: req.body.title }
+    });
+    if (titleExists) {
+      return res.status(400).json({ error: 'This plan already exists.' });
+    }
+
+    // Create plan
     const { id, title, duration, price } = await Plan.create(req.body);
 
     return res.json({
@@ -15,13 +40,38 @@ class PlanController {
   }
 
   async index(req, res) {
+    /**
+     * Show all active Plans
+     */
     const plans = await Plan.findAll();
 
     return res.json(plans);
   }
 
   async update(req, res) {
-    return res.json();
+    /**
+     * Update Plans info
+     */
+    const { title } = req.body;
+    const plan = await Plan.findByPk(req.params.id);
+
+    // if change title
+    if (title !== plan.title) {
+      const titleExists = await Plan.findOne({ where: { title } });
+
+      if (titleExists) {
+        return res.status(400).json({ error: 'This plan already exists.' });
+      }
+    }
+
+    const { id, duration, price } = await plan.update(req.body); // update all the fields
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price
+    });
   }
 
   async delete(req, res) {
