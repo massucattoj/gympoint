5a63142cc26bf53619f3dc737f75548689658fc2
massucattoj <massucattoj@gmail.com>
2019-12-11 10:29:46 -0300

    validation enrollment

--- backend/src/app/controllers/EnrollmentController.js
+++ backend/src/app/controllers/EnrollmentController.js
@@ -1,8 +1,11 @@
 import * as Yup from 'yup';
-import { addMonths, parseISO } from 'date-fns';
+import { addMonths, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Enrollment from '../models/Enrollment';
 import Plan from '../models/Plan';
 import Student from '../models/Student';
+
+import Mail from '../../lib/Mail';
 
 class EnrollmentController {
   async store(req, res) {
@@ -24,7 +27,11 @@ class EnrollmentController {
 
     const { student_id, plan_id, start_date } = req.body;
 
-    // find plan to get infos about duration and price
+    /**
+     * Student need exist
+     * Plan Need exist
+     * Enrollment day doesn't be a past date
+     */
     const plan = await Plan.findByPk(plan_id);
     if (!plan) {
       return res.status(400).json({ error: 'Plan does not exist' });
@@ -33,6 +40,25 @@ class EnrollmentController {
     const student = await Student.findByPk(student_id);
     if (!student) {
       return res.status(400).json({ error: 'Student does not exist' });
+    }
+
+    if (isBefore(parseISO(start_date), new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+    //
+
+    // last validation -> student already have an active plan
+    const validateEnrollment = await Enrollment.findOne({
+      where: {
+        student_id,
+        plan_id,
+        end_data: isBefore(parseISO(start_date), Enrollment.end_date)
+      }
+    });
+    if (validateEnrollment) {
+      return res
+        .status(400)
+        .json({ error: 'Student already have an active Plan' });
     }
 
     const end_date = addMonths(parseISO(start_date), plan.duration);
@@ -45,6 +71,19 @@ class EnrollmentController {
       end_date,
       price
     });
+
+    // // send email!
+    // await Mail.sendMail({
+    //   to: `${student.name} <${student.email}>`,
+    //   subject: 'Matricula realizada com Sucesso. Bem-vindo(a)!',
+    //   template: 'enrollment',
+    //   context: {
+    //     student: student.name,
+    //     plan: plan.title,
+    //     end_date: format(end_date, "'Dia' dd 'de' MMMM'", { locale: pt }),
+    //     price
+    //   }
+    // });
 
     return res.json(enrollment);
   }
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '279bc44321324c',
+    pass: 'd7e4a6bfcd5c32'
+  },
+  default: {
+    from: 'Equipe GymPoint <noreply@gympoint.com>'
+  }
+};
