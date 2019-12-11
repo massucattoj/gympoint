c5492df3c7a7e6457d9d3c0ce904cbb54112318d
massucattoj <massucattoj@gmail.com>
2019-12-11 14:57:55 -0300

    CRUD Enrollment

--- backend/src/app/controllers/EnrollmentController.js
+++ backend/src/app/controllers/EnrollmentController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import { addMonths, parseISO, isBefore, format } from 'date-fns';
+import { Op } from 'sequelize';
 import pt from 'date-fns/locale/pt';
 import Enrollment from '../models/Enrollment';
 import Plan from '../models/Plan';
@@ -31,6 +32,7 @@ class EnrollmentController {
      * Student need exist
      * Plan Need exist
      * Enrollment day doesn't be a past date
+     * Student already have an active plan
      */
     const plan = await Plan.findByPk(plan_id);
     if (!plan) {
@@ -45,14 +47,13 @@ class EnrollmentController {
     if (isBefore(parseISO(start_date), new Date())) {
       return res.status(400).json({ error: 'Past dates are not permitted' });
     }
-    //
 
-    // last validation -> student already have an active plan
     const validateEnrollment = await Enrollment.findOne({
       where: {
         student_id,
-        plan_id,
-        end_data: isBefore(parseISO(start_date), Enrollment.end_date)
+        end_date: {
+          [Op.gt]: parseISO(start_date) // greater then start_date
+        }
       }
     });
     if (validateEnrollment) {
@@ -61,6 +62,7 @@ class EnrollmentController {
         .json({ error: 'Student already have an active Plan' });
     }
 
+    // Create Enrollment
     const end_date = addMonths(parseISO(start_date), plan.duration);
     const price = plan.duration * plan.price;
 
@@ -86,6 +88,89 @@ class EnrollmentController {
     // });
 
     return res.json(enrollment);
+  }
+
+  async index(req, res) {
+    /**
+     * Show all the enrollments
+     */
+    const enrollments = await Enrollment.findAll();
+
+    return res.json(enrollments);
+  }
+
+  async update(req, res) {
+    /**
+     * Yup validation schema
+     */
+    const schema = Yup.object().shape({
+      student_id: Yup.number().positive(),
+      plan_id: Yup.number().positive(),
+      start_date: Yup.date()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(400)
+        .json({ error: 'Validation fails - Cannot Update!' });
+    }
+
+    const enrollment = await Enrollment.findByPk(req.params.id);
+    if (!enrollment) {
+      return res.status(400).json({ error: 'Enrolmment does not exists' });
+    }
+
+    const plan = await Plan.findByPk(req.body.plan_id);
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exists' });
+    }
+
+    const student = await Student.findByPk(req.body.student_id);
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exists' });
+    }
+
+    /**
+     * If body.start_date diff from enrollment.start_date and
+     * body.start_date is not a past date
+     * then change enrollment start_date and end_date is calculated auto
+     */
+    if (
+      req.body.start_date !== enrollment.start_date &&
+      !isBefore(parseISO(req.body.start_date), new Date())
+    ) {
+      const new_end = addMonths(parseISO(req.body.start_date), plan.duration);
+      req.body.end_date = new_end;
+    }
+
+    const {
+      id,
+      student_id,
+      plan_id,
+      start_date,
+      end_date,
+      price
+    } = await enrollment.update(req.body);
+
+    return res.json({
+      id,
+      student_id,
+      plan_id,
+      start_date,
+      end_date,
+      price
+    });
+  }
+
+  async delete(req, res) {
+    /**
+     * Delete enrollment
+     */
+    await Enrollment.destroy({
+      where: { id: req.params.id }
+    });
+
+    return res.json({ error: 'Enrollment deleted with successful!' });
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -22,10 +22,13 @@ routes.put('/students/:id', StudentController.update); // edit user
 // plans
 routes.post('/plan', PlanController.store); // create plan
 routes.get('/plans', PlanController.index); // show plans
-routes.put('/plans/:id', PlanController.update); // update plan
-routes.delete('/plans/:id', PlanController.delete); // delete plan
+routes.put('/plan/:id', PlanController.update); // update plan
+routes.delete('/plan/:id', PlanController.delete); // delete plan
 
 // enrollments
 routes.post('/enrollment', EnrollmentController.store); // create enrollment
+routes.get('/enrollments', EnrollmentController.index);
+routes.put('/enrollment/:id', EnrollmentController.update);
+routes.delete('/enrollment/:id', EnrollmentController.delete);
 
 export default routes;
